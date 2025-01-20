7e999cb6b7fbf99a6e6a8e329cf3202d0d8ca790
Jakobbb <149049733+JakobChi@users.noreply.github.com>
2025-01-20 14:11:06 +0100

    Update Zwischenrufe.tsx

--- pages/Zwischenrufe.tsx
+++ pages/Zwischenrufe.tsx
@@ -2,17 +2,58 @@ import Navbar from '../components/navbar';
 import Footer from '../components/footer';
 import Hero from '../components/hero';
 import Header from '../components/header';
+import PersonenDiagramm from '../components/p5-scripte/zwischenrufer';
+import ParteienDiagramm from '../components/p5-scripte/parteien';
+
 
 export default function Zwischenrufe() {
   return (
     <div>
-        <title>Bundestagsscraper</title>
-        <meta name="description" content="A website inspired by the Bundestag displaying curated data." />
+      <title>Bundestagsscraper</title>
+      <meta
+        name="description"
+        content="A website inspired by the Bundestag displaying curated data."
+      />
       <Header />
       <Navbar />
+
       <main>
-        <p>Das ist die Seite für Zwischenrufe. Hier werden die Daten für die Zwischenrufe stehen.</p>
-        <Hero/>
+        <h1 class='Home-module__g21JLG__diagramquestion'>Wer ruft am meisten dazwischen?</h1>
+
+        {/* Grid-Container mit eindeutigem Klassennamen */}
+        <div className="Home-module__g21JLG__diagram-grid">
+          <div className="Home-module__g21JLG__grid-header">
+            <h2>Personen</h2>
+          </div>
+          <div className="Home-module__g21JLG__grid-header">
+            <h2>Partei</h2>
+          </div>
+          <div className="Home-module__g21JLG__grid-item">
+            <PersonenDiagramm />
+          </div>
+          <div className="Home-module__g21JLG__grid-item">
+            <ParteienDiagramm />
+          </div>
+        </div>
+
+        <h1 class='Home-module__g21JLG__diagramquestion'>Wer ruft am meisten dazwischen?</h1>
+        {/* Grid-Container mit eindeutigem Klassennamen */}
+        <div className="Home-module__g21JLG__diagram-grid">
+          <div className="Home-module__g21JLG__grid-header">
+            <h2>Personen</h2>
+          </div>
+          <div className="Home-module__g21JLG__grid-header">
+            <h2>Partei</h2>
+          </div>
+          <div className="Home-module__g21JLG__grid-item">
+            <PersonenDiagramm />
+          </div>
+          <div className="Home-module__g21JLG__grid-item">
+            <ParteienDiagramm />
+          </div>
+        </div>
+        <h1 class='Home-module__g21JLG__diagramquestion'>TODO</h1>
+        <Hero />
       </main>
     </div>
   );
